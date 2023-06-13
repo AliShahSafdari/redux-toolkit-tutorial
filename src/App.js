3273e066aa53b51737319fa1331e19951eeab893
Alishah Safdari <alishahsafdari@gmail.com>
2023-06-13 08:51:09 +0430

    Add clear all cart

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Navbar from "./conponents/Navbar";
+import CartContainer from "./conponents/CartContainer";
+
+function App() {
+  return <main><Navbar/>
+    <CartContainer/>
+  </main>;
+}
+export default App;
