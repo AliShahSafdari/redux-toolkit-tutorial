28efe76131dc0701d91008c060a26e3832493656
Alishah Safdari <alishahsafdari@gmail.com>
2023-06-13 10:44:09 +0430

    Add API part

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import { useEffect } from 'react';
 import Navbar from './components/Navbar'
 import CartContainer from './components/CartContainer';
 import { useSelector, useDispatch } from 'react-redux';
-import { calculateTotals } from './features/cart/cartSlice';
+import { calculateTotals, getCartItems  } from './features/cart/cartSlice';
 import Modal from './components/Modal';
 
 function App() {
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
   const { isOpen } = useSelector((state) => state.modal);
 
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main>
